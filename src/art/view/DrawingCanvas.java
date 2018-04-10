c5f80bce1bc81a3ea7a70e6c3d2b8cb2a02399a7
wilshdog <31513793+wilshdog@users.noreply.github.com>
2018-04-10 09:42:38 -0600

    started filling out the stuubs of drawing canvas.

--- src/art/view/DrawingCanvas.java
+++ src/art/view/DrawingCanvas.java
@@ -32,7 +32,75 @@ public class DrawingCanvas extends JPanel
 	
 	public ShapeCanvas(ArtController app)
 	{
+		super();
+		this.app = app;
+		triangleList = new ArrayList<Polygon>();
+		polygonList = new ArrayList<Polygon>();
+		ellipseList = new ArrayList<Polygon>();
+		rectangleList = new ArrayList<Rectangle>();
 		
+		canvasImage = new BufferedImage(600, 600, BufferedImage.TYPE_INT_ARGB);
+		this.setMinimumSize(new Dimension(600, 600));
+		this.setPreferredSize(new Dimension(600, 600));
+		this.setMaximumSize(getPreferredSize());
+	}
+	
+	public void addShape(Shape current)
+	{
+		if (current instanceof Polygon)
+		{
+			if(((Polygon)current).xpoints.length == 3)
+			{
+				triangleList.add((Polygon) current);
+			}
+			else
+			{
+				polygonList.add((Polygon)current);
+			}
+		}
+		else if (current instanceof Ellipse2D)
+		{
+			ellipseList.add((Ellipse2D)current);
+			
+		}
+		else 
+		{
+			rectangleList.add((Rectangle) current);
+		}
+		updateImage();
+		
+	}
+	
+	public void clear()
+	{
+		
+		
+	}
+	
+	public void changeBackground()
+	{
+		
+	}
+	
+	public void save()
+	{
+		
+	}
+	
+	public Color randomColor()
+	{
+		
+	}
+	
+	private void updateImage()
+	{
+		
+	}
+	
+	protected void paintComponent(Graphics graphics)
+	{
+		super.paintComponent(graphics);
+		graphics.drawImage(canvasImage, 0, 0, null);
 		
 	}
 
